fe603ae08b546532ca1e6d2af2b9eadf2eeb4922
= <guillaume.blas4@gmail.com>
2025-01-09 14:33:14 +0100

    Product + Sales CTRL

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -1,5 +1,25 @@
 package controllers;
 
-public class ProductController {
+import javax.swing.JOptionPane;
 
+import models.Product;
+import models.DAO.ProductDAO;
+import views.ProductView;
+
+public class ProductController {
+    private ProductView vue;
+    private ProductDAO ProductDAO;
+
+    public ProductController(ProductView vue, ProductDAO ProductDAO) {
+        this.vue = vue;
+        this.ProductDAO = ProductDAO;
+
+        this.vue.setAddProductListener(e -> {
+            String nom = vue.getProductName();
+            Product Product = new Product(nom, 0, 0, null, null, null);
+
+            ProductDAO.addProduct(Product);
+            JOptionPane.showMessageDialog(null, "Catégorie ajoutée !");
+        });
+    }
 }
--- src/controllers/SaleController.java
+++ src/controllers/SaleController.java
@@ -1,5 +1,30 @@
 package controllers;
 
+import java.sql.Date;
+
+import javax.swing.JOptionPane;
+
+import models.Product;
+import models.Sale;
+import models.DAO.SaleDAO;
+import views.SaleView;
+
 public class SaleController {
-    
+    private SaleView vue;
+    private SaleDAO SaleDAO;
+
+    public SaleController(SaleView vue, SaleDAO SaleDAO) {
+        this.vue = vue;
+        this.SaleDAO = SaleDAO;
+
+        this.vue.setAddSaleListener(e -> {
+            java.util.Date date = vue.getDate();
+            Integer quantity = vue.getQuantity();
+
+            Sale sale = new Sale(quantity, date, null);
+
+            SaleDAO.addSale(sale);
+            JOptionPane.showMessageDialog(null, "Catégorie ajoutée !");
+        });
+    }
 }
