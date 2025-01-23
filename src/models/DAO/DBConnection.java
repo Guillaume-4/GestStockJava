155fa34e4a6504e96621fe5e732ce24657860d04
svasco <76670937+semanavasco@users.noreply.github.com>
2025-01-23 22:39:52 +0100

    Patched DBConnection
    Fixed "the connection is closed" error when performing multiple queries.

--- src/models/DAO/DBConnection.java
+++ src/models/DAO/DBConnection.java
@@ -4,23 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static String URL = "jdbc:sqlserver://163.5.143.146:1433;databaseName=GestStockJava;encrypt=false";
-    private static String USER = "connect";
-    private static String PASSWORD = "?5Q7_53RV3R?*";
-    private static Connection connection;
+    private static final String URL = "jdbc:sqlserver://163.5.143.146:1433;databaseName=GestStockJava;encrypt=false";
+    private static final String USER = "connect";
+    private static final String PASSWORD = "?5Q7_53RV3R?*";
 
     private DBConnection() {}
 
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            } catch (Exception e) {
-                System.out.println("Erreur de connexion à la base de données : " + e.getMessage());
-            }
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Erreur de connexion à la base de données : " + e.getMessage());
+            return null;
         }
-        return connection;
     }
 }
