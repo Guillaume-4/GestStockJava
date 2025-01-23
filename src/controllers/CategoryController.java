d0996a4912bfab838a913e0c4716e52f4cbbffe7
svasco <76670937+semanavasco@users.noreply.github.com>
2025-01-23 22:45:27 +0100

    Patched controllers
    Fixed naming, added reports to sales

--- src/controllers/CategoryController.java
+++ src/controllers/CategoryController.java
@@ -7,16 +7,16 @@ import models.DAO.CategoryDAO;
 import views.CategoryView;
 
 public class CategoryController {
-    private CategoryView vue;
+    private CategoryView view;
     private CategoryDAO categoryDAO;
 
-    public CategoryController(CategoryView vue, CategoryDAO categoryDAO) {
-        this.vue = vue;
+    public CategoryController(CategoryView view, CategoryDAO categoryDAO) {
+        this.view = view;
         this.categoryDAO = categoryDAO;
 
-        this.vue.setAddCategoryListener(e -> {
-            String nom = vue.getCategoryName();
-            Category category = new Category(nom);
+        this.view.setAddCategoryListener(_ -> {
+            String name = view.getCategoryName();
+            Category category = new Category(name);
 
             categoryDAO.addSale(category);
             JOptionPane.showMessageDialog(null, "Catégorie ajoutée !");
--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -1,25 +1,64 @@
 package controllers;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
+import models.Category;
+import models.Furnisher;
 import models.Product;
+import models.Sale;
+import models.DAO.CategoryDAO;
+import models.DAO.FurnisherDAO;
 import models.DAO.ProductDAO;
+import models.DAO.SaleDAO;
 import views.ProductView;
 
 public class ProductController {
-    private ProductView vue;
-    private ProductDAO ProductDAO;
+    private ProductView view;
+    private ProductDAO productDAO;
+    private SaleDAO saleDAO;
 
-    public ProductController(ProductView vue, ProductDAO ProductDAO) {
-        this.vue = vue;
-        this.ProductDAO = ProductDAO;
+    public ProductController(ProductView view, ProductDAO productDAO, SaleDAO saleDAO) {
+        this.view = view;
+        this.productDAO = productDAO;
+        this.saleDAO = saleDAO;
 
-        this.vue.setAddProductListener(e -> {
-            String nom = vue.getProductName();
-            Product Product = new Product(nom, 0, 0, null, null, null);
+        this.view.setAddProductListener(_ -> {
+            String name = view.getProductName();
+            int quantity = view.getProductQuantity();
+            double unitPrice = view.getProductUnitPrice();
+            String categoryName = view.getProductCategoryName();
+            String furnisherName = view.getProductFurnisherName();
+            Category category = new CategoryDAO().getCategoryByName(categoryName);
+            Furnisher furnisher = new FurnisherDAO().getFurnisherByName(furnisherName);
 
-            ProductDAO.addProduct(Product);
-            JOptionPane.showMessageDialog(null, "Catégorie ajoutée !");
+            Product product = new Product(name, quantity, unitPrice, category, furnisher, null);
+
+            productDAO.addProduct(product);
+
+            JOptionPane.showMessageDialog(null, "Produit ajouté !");
         });
+    }
+
+    public void generateReport(String productName, LocalDate startDate, LocalDate endDate) {
+        Product product = new ProductDAO().getProductByName(productName);
+
+        if (product != null) {
+            Date start = Date.valueOf(startDate);
+            Date end = Date.valueOf(endDate);
+
+            List<Sale> sales = new SaleDAO().getSalesByProductAndDateRange(product.getProductId(), start, end);
+            int totalSales = 0;
+
+            for (Sale sale : sales)
+                totalSales += sale.getSaleQuantity();
+
+            view.showReport(productName, product.getProductUnitPrice(), totalSales);
+        } else {
+            JOptionPane.showMessageDialog(null, "Produit non trouvé !");
+        }
     }
 }
