07420de0706cb45a69dde760d9c8c7f99219989f
svasco <76670937+semanavasco@users.noreply.github.com>
2025-02-10 09:14:36 +0100

    Update AppView.java
    - Adds window's title as a label on the page.

--- src/views/components/AppView.java
+++ src/views/components/AppView.java
@@ -1,10 +1,14 @@
 package views.components;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class AppView extends JFrame {
     public GridBagConstraints gbc;
@@ -19,5 +23,17 @@ public class AppView extends JFrame {
 
         gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 10, 5, 10);
+
+        // Title
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 20));
+        add(titleLabel, gbc);
+
+        // Empty Space
+        gbc.gridy = 1;
+        add(Box.createRigidArea(new Dimension(0, 10)), gbc);
     }
 }
