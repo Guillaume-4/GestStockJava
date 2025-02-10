2cb7ba040995c9ebb25cad0b432807db9d693243
svasco <76670937+semanavasco@users.noreply.github.com>
2025-02-10 09:06:21 +0100

    Create ProductListView.java
    - Allows users to view the products;
    - Search and refresh functionality.

---
+++ src/views/ProductListView.java
@@ -0,0 +1,117 @@
+package views;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import models.AppUser;
+import models.Product;
+import models.DAO.ProductDAO;
+import views.components.AppView;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
+public class ProductListView extends AppView {
+    private JTextField searchField;
+    private DefaultTableModel tableModel;
+    private JTable productTable;
+    private JButton backBtn;
+    private JButton refreshBtn;
+    private List<Product> products;
+    private AppUser user;
+
+    public ProductListView(AppUser user) {
+        super("Liste des produits", 800, 600, true);
+        this.user = user;
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+
+        // Search
+        searchField = new JTextField(20);
+        searchField.setToolTipText("Recherchez un produit..");
+        add(searchField, gbc);
+
+        // Table
+        String[] columnNames = { "ID", "Nom", "Quantité", "Prix Unitaire", "Catégorie", "Fournisseur" };
+        tableModel = new DefaultTableModel(columnNames, 0);
+        productTable = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(productTable);
+
+        loadProducts();
+
+        gbc.gridy = 1;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        add(scrollPane, gbc);
+
+        // Buttons
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.gridwidth = 1;
+        gbc.weighty = 0;
+        gbc.weightx = 0;
+        gbc.gridy = 2;
+
+        gbc.gridx = 0;
+        refreshBtn = new JButton("Rafraîchir");
+        add(refreshBtn, gbc);
+
+        gbc.gridx = 1;
+        backBtn = new JButton("Retour");
+        add(backBtn, gbc);
+
+        // Interactions
+        searchField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                filterProducts();
+            }
+        });
+
+        refreshBtn.addActionListener(e -> loadProducts());
+
+        backBtn.addActionListener(e -> {
+            dispose();
+            new ManageProductView(user);
+        });
+
+        setVisible(true);
+    }
+
+    private void loadProducts() {
+        tableModel.setRowCount(0);
+
+        products = new ProductDAO().getAllProducts();
+
+        for (Product product : products)
+            tableModel.addRow(new Object[] {
+                    product.getProductId(),
+                    product.getProductName(),
+                    product.getProductQuantity(),
+                    product.getProductUnitPrice(),
+                    product.getCategory().getCategoryName(),
+                    product.getFurnisher().getFurnisherName()
+            });
+    }
+
+    private void filterProducts() {
+        tableModel.setRowCount(0);
+
+        String searchString = searchField.getText().trim().toLowerCase();
+
+        for (Product product : products)
+            if (product.getProductName().toLowerCase().contains(searchString))
+                tableModel.addRow(new Object[] {
+                        product.getProductId(),
+                        product.getProductName(),
+                        product.getProductQuantity(),
+                        product.getProductUnitPrice(),
+                        product.getCategory().getCategoryName(),
+                        product.getFurnisher().getFurnisherName()
+                });
+    }
+}
