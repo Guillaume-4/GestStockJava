0afe6414b5f11672bb462a84b679eca317748490
svasco <76670937+semanavasco@users.noreply.github.com>
2025-02-10 09:08:12 +0100

    Update ProductView.java
    - Added back button;
    - Added numeric filters to unit price and quantity text fields;
    - Added back button and confirmation;
    - Extends AppView;
    - Added styling;
    - Improved overall reasoning.

--- src/views/ProductView.java
+++ src/views/ProductView.java
@@ -1,40 +1,49 @@
 package views;
 
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
 
+import models.AppUser;
 import models.Category;
 import models.Furnisher;
 import models.DAO.CategoryDAO;
 import models.DAO.FurnisherDAO;
+import views.components.AppView;
+import views.components.NumericFilter;
 import models.Product;
 
-import java.awt.FlowLayout;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-public class ProductView {
-    private JFrame frame;
-
+public class ProductView extends AppView {
     private JTextField nameTxtField;
     private JTextField quantityTxtField;
     private JTextField unitPriceTxtField;
     private JComboBox<String> categorySelector;
     private JComboBox<String> furnisherSelector;
-
     private JButton productActionBtn;
-
+    private JButton backBtn;
     private Product product;
+    private AppUser user;
 
     // Constructors
-    public ProductView() {
-        this(null);
-    }
-
-    public ProductView(Product product) {
+    public ProductView(AppUser user, Product product) {
+        super("Gestion des Produits", 600, 600, false);
+        this.user = user;
         this.product = product;
-        frame = new JFrame("Gestion des Produits");
-        frame.setLayout(new FlowLayout());
-        frame.setSize(400, 300);
+
+        // Title
+        gbc.gridwidth = 2;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JLabel titleLabel = new JLabel(this.product == null ? "Ajout de Produit" : "Modification de Produit");
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 20));
+        add(titleLabel, gbc);
 
         // Category list
         List<Category> categories = new CategoryDAO().getCategories();
@@ -50,53 +59,106 @@ public class ProductView {
         for (int i = 0; i < furnishers.size(); i++)
             furnisherNames[i] = furnishers.get(i).getFurnisherName();
 
-        // Add product fields
-        JLabel nameLabel = new JLabel("Nom du Produit :");
+        // Empty Space
+        gbc.gridy = 1;
+        add(Box.createRigidArea(new Dimension(0, 10)), gbc);
+
+        // Name
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        JLabel nameLabel = new JLabel("Nom du Produit");
+        add(nameLabel, gbc);
+
+        gbc.gridy = 3;
         nameTxtField = new JTextField(20);
+        add(nameTxtField, gbc);
 
+        // Quantity
+        gbc.gridy = 4;
         JLabel quantityLabel = new JLabel("Quantité :");
+        add(quantityLabel, gbc);
+
+        gbc.gridy = 5;
         quantityTxtField = new JTextField(20);
+        ((AbstractDocument) quantityTxtField.getDocument()).setDocumentFilter(new NumericFilter(false));
+        add(quantityTxtField, gbc);
 
+        // Unit Price
+        gbc.gridy = 6;
         JLabel unitPriceLabel = new JLabel("Prix :");
+        add(unitPriceLabel, gbc);
+
+        gbc.gridy = 7;
         unitPriceTxtField = new JTextField(20);
+        ((AbstractDocument) unitPriceTxtField.getDocument()).setDocumentFilter(new NumericFilter(true));
+        add(unitPriceTxtField, gbc);
 
+        // Category
+        gbc.gridy = 8;
         JLabel categoryLabel = new JLabel("Catégorie :");
-        categorySelector = new JComboBox<String>(categoryNames);
+        add(categoryLabel, gbc);
 
+        // Furnisher
+        gbc.gridy = 10;
         JLabel furnisherLabel = new JLabel("Fournisseur :");
+        add(furnisherLabel, gbc);
+
+        // Selectors
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        gbc.gridy = 9;
+        categorySelector = new JComboBox<String>(categoryNames);
+        add(categorySelector, gbc);
+
+        gbc.gridy = 11;
         furnisherSelector = new JComboBox<String>(furnisherNames);
+        add(furnisherSelector, gbc);
 
-        productActionBtn = new JButton("Ajouter Produit");
+        // Empty Space
+        gbc.gridy = 12;
+        add(Box.createRigidArea(new Dimension(0, 10)), gbc);
 
-        frame.add(nameLabel);
-        frame.add(nameTxtField);
-        frame.add(quantityLabel);
-        frame.add(quantityTxtField);
-        frame.add(unitPriceLabel);
-        frame.add(unitPriceTxtField);
-        frame.add(categoryLabel);
-        frame.add(categorySelector);
-        frame.add(furnisherLabel);
-        frame.add(furnisherSelector);
-        frame.add(productActionBtn);
+        // Product Action Button
+        gbc.gridy = 13;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.fill = GridBagConstraints.NONE;
+        productActionBtn = new JButton(this.product == null ? "Ajouter Produit" : "Modifier Produit");
+        productActionBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        add(productActionBtn, gbc);
 
+        // Back Button
+        gbc.gridx = 1;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        backBtn = new JButton("Retour");
+        backBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        add(backBtn, gbc);
+
+        // Fields Initialization if necessary
         if (this.product != null) {
             nameTxtField.setText(product.getProductName());
-
             quantityTxtField.setText(String.valueOf(product.getProductQuantity()));
-
             unitPriceTxtField.setText(String.valueOf(product.getProductUnitPrice()));
-
             categorySelector.setSelectedItem(product.getCategory().getCategoryName());
-
             furnisherSelector.setSelectedItem(product.getFurnisher().getFurnisherName());
-
-            productActionBtn.setText("Modifier Produit");
         }
 
-        frame.setLocation(800, 500);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setVisible(true);
+        // Interactions
+        backBtn.addActionListener(e -> {
+            int response = JOptionPane.showConfirmDialog(
+                    this,
+                    "Voulez-vous vraiment revenir en arrière ? Les modifications seront perdues.",
+                    "Confirmation",
+                    JOptionPane.OK_CANCEL_OPTION);
+
+            if (response == JOptionPane.CANCEL_OPTION)
+                return;
+
+            dispose();
+            new ManageProductView(user);
+        });
+
+        setVisible(true);
     }
 
     // Getters
@@ -105,10 +167,16 @@ public class ProductView {
     }
 
     public int getProductQuantity() {
+        if (quantityTxtField.getText().isEmpty())
+            return 0;
+
         return Integer.parseInt(quantityTxtField.getText());
     }
 
     public double getProductUnitPrice() {
+        if (unitPriceTxtField.getText().isEmpty())
+            return 0;
+
         return Double.parseDouble(unitPriceTxtField.getText());
     }
 
